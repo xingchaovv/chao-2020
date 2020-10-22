fc658b3798d4f640a174179f0f6b18c9099a95b6
Xingchao Zhang <xingchaovv@gmail.com>
2020-10-22 22:46:10 +0800

    初始化主题。

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# chao-2020
+# 个人博客主题 Chao 2020
+
+个人博客：http://xingchaovv.com
+
+基于主题：https://wordpress.org/themes/twentytwenty
---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php
+add_action('wp_enqueue_scripts', function(){
+	wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
+});
